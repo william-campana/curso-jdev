5ffe1d5afc7cdc40ef99a89cbe60d9d6323b1bb7
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-09 19:45:37 -0300

    Teste do Git

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,6 +4,7 @@ public class PrimeiraClasseJava {
 	
 	public static void main (String[] args) {
 		System.out.println("Primeiro Programa em Java");
+		System.out.println("Inicido do Curso");
 	}
 
 }
