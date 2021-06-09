0f233b66a475ac1842a12d84d78777a9211a4321
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-09 19:54:15 -0300

    Teste de commit

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,6 +5,8 @@ public class PrimeiraClasseJava {
 	public static void main (String[] args) {
 		System.out.println("Primeiro Programa em Java");
 		System.out.println("Inicido do Curso");
+		System.out.println("Inicido do Curso");
+		System.out.println("Inicido do Curso");
 	}
 
 }
