d99f073c05f32ad5e04e7d51c62da2b3673116ad
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-13 20:18:42 -0300

    Operações com Char

---
+++ primeiro-programa-java/src/cursojava/executavel/OperacoesComChar.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class OperacoesComChar {
+
+	public static void main(String[] args) {
+		char tipoPessoa = 'F';  // tipo char sempre aspas simples
+		
+		System.out.println("Tipo Pessoa: " + tipoPessoa);
+	}
+
+}
