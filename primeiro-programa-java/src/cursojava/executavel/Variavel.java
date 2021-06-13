a1e6444b93f090f88ea7714c5f275df7e1641a86
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-13 17:04:31 -0300

    Variaveis

---
+++ primeiro-programa-java/src/cursojava/executavel/Variavel.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main (String[] args) {
+		
+		byte _byte = 10;
+		short _short = 100;
+		int _int = 1000;
+		long _long = 1000L;
+		boolean _boolean = true;
+		String _string = "Qualquer String"
+	}
+}
