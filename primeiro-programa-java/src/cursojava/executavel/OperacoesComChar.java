b44262d0664c3b8760b13cf9f3e9d1704ae4d4c4
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-13 20:20:11 -0300

    Comentario

--- primeiro-programa-java/src/cursojava/executavel/OperacoesComChar.java
+++ primeiro-programa-java/src/cursojava/executavel/OperacoesComChar.java
@@ -3,7 +3,7 @@ package cursojava.executavel;
 public class OperacoesComChar {
 
 	public static void main(String[] args) {
-		char tipoPessoa = 'F';  // tipo char sempre aspas simples
+		char tipoPessoa = 'F';  // tipo char sempre aspas simples e somente 1 caracter
 		
 		System.out.println("Tipo Pessoa: " + tipoPessoa);
 	}
