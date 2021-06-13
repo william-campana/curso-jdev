b8dcc1d30989768e83932760130bd20cd4d84e62
william.campana <william.campana@DESKTOP-R7IP06N>
2021-06-13 20:18:18 -0300

    Operações com Numeros

---
+++ primeiro-programa-java/src/cursojava/executavel/OperacoesComNumero.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+public class OperacoesComNumero {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		int totalNota = nota1 + nota2 + nota3 + nota4;
+		double media = totalNota / 4f;
+		
+		
+		System.out.println("Média das notas: " + media);
+				
+
+	}
+
+}
